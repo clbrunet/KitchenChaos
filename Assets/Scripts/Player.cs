8e370725eeac6fbb4c7ac681e5a30033f5e1cc8a
clbrunet <brunet.clement5@gmail.com>
2023-03-02 10:19:54 +0100

    feat: add a player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    private float rotateSpeed = 10f;
+
+    private void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+        inputVector.Normalize();
+        Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        if (moveDirection != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+        }
+    }
+}
