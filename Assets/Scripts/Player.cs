3fbe7ca9743bbf18ad8cba3160317ddf22e12926
clbrunet <brunet.clement5@gmail.com>
2023-03-02 17:48:41 +0100

    feat: add player interactions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,19 @@ public class Player : MonoBehaviour
     [SerializeField] private GameInput gameInput;
 
     [SerializeField] private float moveSpeed = 7f;
-    private readonly float rotateSpeed = 10f;
-    private readonly float playerHeight = 2.1f;
-    private readonly float playerRadius = 0.65f;
+
+    [SerializeField] private LayerMask countersLayerMask;
+    private Vector3 lastInteractionsDirection = Vector3.zero;
 
     private bool isWalking = false;
 
     private void Update()
+    {
+        HandleMovements();
+        HandleInteractions();
+    }
+
+    private void HandleMovements()
     {
         Vector2 inputVector = gameInput.GetNormalizedInputVector();
         isWalking = inputVector != Vector2.zero;
@@ -21,7 +27,8 @@ public class Player : MonoBehaviour
         {
             return;
         }
-        transform.forward = Vector3.Slerp(transform.forward, new Vector3(inputVector.x, 0f, inputVector.y), rotateSpeed * Time.deltaTime);
+        const float ROTATE_SPEED = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, new Vector3(inputVector.x, 0f, inputVector.y), ROTATE_SPEED * Time.deltaTime);
         float moveDistance = moveSpeed * Time.deltaTime;
         TryMove(new Vector3(inputVector.x, 0f, 0f), moveDistance);
         TryMove(new Vector3(0f, 0f, inputVector.y), moveDistance);
@@ -29,11 +36,30 @@ public class Player : MonoBehaviour
 
     private void TryMove(Vector3 moveDirection, float moveDistance)
     {
-        bool canMove = !Physics.CapsuleCast(transform.position + playerRadius * Vector3.up,
-            transform.position + (playerHeight - playerRadius) * Vector3.up, playerRadius, moveDirection, moveDistance);
-        if (canMove) {
+        const float PLAYER_HEIGHT = 2.1f;
+        const float PLAYER_RADIUS = 0.65f;
+        if (!Physics.CapsuleCast(transform.position + PLAYER_RADIUS * Vector3.up,
+            transform.position + (PLAYER_HEIGHT - PLAYER_RADIUS) * Vector3.up, PLAYER_RADIUS, moveDirection, moveDistance))
+        {
             transform.position += moveDistance * moveDirection;
         }
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetNormalizedInputVector();
+        Vector3 direction = new(inputVector.x, 0f, inputVector.y);
+        if (direction != Vector3.zero)
+        {
+            lastInteractionsDirection = direction;
+        }
+        const float INTERACTION_MAX_DISTANCE = 2f;
+        if (!Physics.Raycast(transform.position, lastInteractionsDirection, out RaycastHit raycastHit, INTERACTION_MAX_DISTANCE, countersLayerMask)
+            || !raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+        {
+            return;
+        }
+        clearCounter.Interact();
     }
 
     public bool IsWalking()
