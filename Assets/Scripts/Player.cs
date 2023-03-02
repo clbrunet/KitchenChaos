15663d21d2b504f06c65b7fb5e8e7d36049672c6
clbrunet <brunet.clement5@gmail.com>
2023-03-02 16:38:27 +0100

    feat: add player collision detection using CapsuleCast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,18 +8,31 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
     private readonly float rotateSpeed = 10f;
+    private readonly float playerHeight = 2.1f;
+    private readonly float playerRadius = 0.65f;
 
     private bool isWalking = false;
 
     private void Update()
     {
         Vector2 inputVector = gameInput.GetNormalizedInputVector();
-        Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);
-        transform.position += moveSpeed * Time.deltaTime * moveDirection;
-        isWalking = moveDirection != Vector3.zero;
-        if (isWalking)
+        isWalking = inputVector != Vector2.zero;
+        if (!isWalking)
         {
-            transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+            return;
+        }
+        transform.forward = Vector3.Slerp(transform.forward, new Vector3(inputVector.x, 0f, inputVector.y), rotateSpeed * Time.deltaTime);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        TryMove(new Vector3(inputVector.x, 0f, 0f), moveDistance);
+        TryMove(new Vector3(0f, 0f, inputVector.y), moveDistance);
+    }
+
+    private void TryMove(Vector3 moveDirection, float moveDistance)
+    {
+        bool canMove = !Physics.CapsuleCast(transform.position + playerRadius * Vector3.up,
+            transform.position + (playerHeight - playerRadius) * Vector3.up, playerRadius, moveDirection, moveDistance);
+        if (canMove) {
+            transform.position += moveDistance * moveDirection;
         }
     }
 
