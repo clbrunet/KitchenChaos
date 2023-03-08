05e8b5b796f5ad4a98337ee290e46523d9ad63f1
clbrunet <brunet.clement5@gmail.com>
2023-03-08 09:32:28 +0100

    feat: add the main menu and loading scenes

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFirstUpdateDone = false;
+
+    void Update()
+    {
+        if (!isFirstUpdateDone)
+        {
+            Loader.LoaderCallback();
+        }
+        isFirstUpdateDone = true;
+    }
+}
